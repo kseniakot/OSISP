7dd339c62bbec830157dd8b3f144060d96f1db49
ksu_kotovaa <ksu_kotovaa@mail.ru>
2024-09-19 00:35:41 +0300

    refactored code

--- sort/main.cpp
+++ sort/main.cpp
@@ -6,67 +6,60 @@
 #include <mutex>
 #include <numeric>
 #include <Pdh.h>
-
+#include <atomic>
 
 std::mutex coutMutex;
+std::atomic<bool> monitoringCpu(true);
 
-static PDH_HQUERY cpuQuery;
-static PDH_HCOUNTER cpuTotal;
-bool monitoringCpu = true; // Флаг для завершения потока мониторинга
-std::vector<double> cpuUsageValues; // Вектор для хранения значений загрузки CPU
+class CpuMonitor {
+public:
+    CpuMonitor() {
+        PdhOpenQuery(NULL, 0, &cpuQuery);
+        PdhAddEnglishCounterW(cpuQuery, L"\\Processor(_Total)\\% Processor Time", 0, &cpuTotal);
+        PdhCollectQueryData(cpuQuery);
+        Sleep(1000);
+    }
 
-void initCpuQuery() {
-    PdhOpenQuery(NULL, 0, &cpuQuery);
-    PdhAddEnglishCounterW(cpuQuery, L"\\Processor(_Total)\\% Processor Time", 0, &cpuTotal);
-    PdhCollectQueryData(cpuQuery);
-    Sleep(1000);
-}
+    ~CpuMonitor() {
+        PdhCloseQuery(cpuQuery);
+    }
 
-double getCpuUsage() {
-    PDH_FMT_COUNTERVALUE counterVal;
-    PdhCollectQueryData(cpuQuery);
-    Sleep(1000);
-    PdhGetFormattedCounterValue(cpuTotal, PDH_FMT_DOUBLE, NULL, &counterVal);
-    return counterVal.doubleValue;
-}
+    double getCpuUsage() {
+        PDH_FMT_COUNTERVALUE counterVal;
+        PdhCollectQueryData(cpuQuery);
+        Sleep(1000);
+        PdhGetFormattedCounterValue(cpuTotal, PDH_FMT_DOUBLE, NULL, &counterVal);
+        return counterVal.doubleValue;
+    }
+
+private:
+    PDH_HQUERY cpuQuery{};
+    PDH_HCOUNTER cpuTotal{};
+};
+
+
 
 DWORD WINAPI monitorCpuUsage(LPVOID param) {
+    auto *cpuMonitor = static_cast<CpuMonitor *>(param);
+    std::vector<double> cpuUsageValues;
+
     while (monitoringCpu) {
-        double cpuUsage = getCpuUsage();
+        double cpuUsage = cpuMonitor->getCpuUsage();
         {
             std::lock_guard<std::mutex> lock(coutMutex);
-            cpuUsageValues.push_back(cpuUsage); // Сохраняем значения в вектор
+            cpuUsageValues.push_back(cpuUsage);
         }
-        Sleep(1000); // Замеры каждые 1000 миллисекунд
+        Sleep(1000);
     }
-    return 0;
-}
 
-struct ThreadData {
-    int* array;
-    int left;
-    int right;
-    int threadId;
-};
-
-DWORD WINAPI threadSort(LPVOID param) {
-    ThreadData* data = (ThreadData*)param;
-    std::sort(data->array + data->left, data->array + data->right + 1);
-
+    double averageCpuUsage = std::accumulate(cpuUsageValues.begin(), cpuUsageValues.end(), 0.0) / cpuUsageValues.size();
     std::lock_guard<std::mutex> lock(coutMutex);
-    std::cout << "\nThread " << data->threadId << ": done\n";
+    std::cout << "Average CPU load during execution: " << averageCpuUsage << "%\n";
+
     return 0;
 }
 
-struct ThreadMergeData {
-    int* array;
-    int left;
-    int mid;
-    int right;
-    int threadId;
-};
-
-void merge(int* arr, int left, int mid, int right) {
+void merge(int *arr, int left, int mid, int right) {
     int leftSize = mid - left + 1;
     int rightSize = right - mid;
     std::vector<int> leftArray(leftSize), rightArray(rightSize);
@@ -83,8 +76,25 @@ void merge(int* arr, int left, int mid, int right) {
     while (j < rightSize) arr[k++] = rightArray[j++];
 }
 
+struct ThreadTaskData {
+    int *array;
+    int left;
+    int mid;
+    int right;
+    int threadId;
+};
+
+DWORD WINAPI threadSort(LPVOID param) {
+    auto *data = static_cast<ThreadTaskData *>(param);
+    std::sort(data->array + data->left, data->array + data->right + 1);
+
+    std::lock_guard<std::mutex> lock(coutMutex);
+    std::cout << "\nThread " << data->threadId << ": done\n";
+    return 0;
+}
+
 DWORD WINAPI threadMerge(LPVOID param) {
-    ThreadMergeData* data = (ThreadMergeData*)param;
+    auto *data = static_cast<ThreadTaskData *>(param);
     merge(data->array, data->left, data->mid, data->right);
 
     std::lock_guard<std::mutex> lock(coutMutex);
@@ -92,10 +102,9 @@ DWORD WINAPI threadMerge(LPVOID param) {
     return 0;
 }
 
-void parallelMerge(int* arr, int size, int numThreads) {
+void parallelMerge(int *arr, int size, int numThreads, std::vector<HANDLE>& threads) {
     int chunkSize = size / numThreads;
-    std::vector<HANDLE> threads(numThreads);
-    std::vector<ThreadMergeData> threadData(numThreads);
+    std::vector<ThreadTaskData> threadData(numThreads);
 
     for (int mergeSize = chunkSize; mergeSize < size; mergeSize *= 2) {
         int currentThreads = numThreads / 2;
@@ -107,77 +116,81 @@ void parallelMerge(int* arr, int size, int numThreads) {
             int mid = std::min(left + mergeSize - 1, size - 1);
             int right = std::min(left + 2 * mergeSize - 1, size - 1);
 
-            threadData[i] = { arr, left, mid, right, i };
+            threadData[i] = {arr, left, mid, right, i};
             threads[i] = CreateThread(NULL, 0, threadMerge, &threadData[i], 0, NULL);
         }
 
         WaitForMultipleObjects(currentThreads, threads.data(), TRUE, INFINITE);
-        for (HANDLE& thread : threads) CloseHandle(thread);
+        for (HANDLE &thread: threads) CloseHandle(thread);
 
         numThreads /= 2;
     }
 }
 
-void parallelSort(int* arr, int size, int numThreads) {
-    std::vector<HANDLE> threads(numThreads);
-    std::vector<ThreadData> threadData(numThreads);
+void parallelSort(int *arr, int size, int numThreads, std::vector<HANDLE>& threads) {
+    std::vector<ThreadTaskData> threadData(numThreads);
 
     int chunkSize = size / numThreads;
 
-    // Фаза сортировки: каждый поток сортирует свою часть
+
     for (int i = 0; i < numThreads; ++i) {
         int left = i * chunkSize;
         int right = (i == numThreads - 1) ? size - 1 : (left + chunkSize - 1);
-        threadData[i] = { arr, left, right, i };
+        threadData[i] = {arr, left, 0, right, i}; // mid не используется для сортировки
         threads[i] = CreateThread(NULL, 0, threadSort, &threadData[i], 0, NULL);
     }
 
     WaitForMultipleObjects(numThreads, threads.data(), TRUE, INFINITE);
-    for (HANDLE& thread : threads) CloseHandle(thread);
-
-    // Фаза слияния: многопоточное слияние отсортированных частей
-    parallelMerge(arr, size, numThreads);
 }
 
-int main() {
-    int size = 1'000'000'000, numThreads = 1;
-    std::cout << "Array size: " << size << "\n";
-    std::cout << "Number of threads: " << numThreads << "\n";
+void sortAndMerge(int *arr, int size, int numThreads) {
+    std::vector<HANDLE> threads(numThreads);
 
-    if (numThreads < 1 || size < numThreads) {
-        std::cout << "Invalid number of threads or array size.\n";
-        return -1;
+    parallelSort(arr, size, numThreads, threads);
+    parallelMerge(arr, size, numThreads, threads);
+}
+
+
+int main() {
+    int size = 1'000'000'000, numThreads = 0;
+    std::cout << "Array size: " << size << "\n";
+    std::cin >> size;
+
+    while (numThreads <= 0 || numThreads > 64) {
+        std::cout << "Number of threads (1-64): ";
+        std::cin >> numThreads;
     }
+
 
     std::vector<int> array(size);
     for (int j = 0; j < size; j++) array[j] = j % 10;
 
-    initCpuQuery();
+    CpuMonitor cpuMonitor;
 
-    double initialCpuUsage = getCpuUsage();
+    double initialCpuUsage = cpuMonitor.getCpuUsage();
     std::cout << "CPU load before start: " << initialCpuUsage << "%\n";
 
-    // Запуск мониторинга CPU в отдельном потоке
-    HANDLE cpuMonitorThread = CreateThread(NULL, 0, monitorCpuUsage, NULL, 0, NULL);
+    HANDLE cpuMonitorThread = CreateThread(NULL, 0, monitorCpuUsage, &cpuMonitor, 0, NULL);
 
     auto start = std::chrono::high_resolution_clock::now();
-    parallelSort(array.data(), size, numThreads);
+    sortAndMerge(array.data(), size, numThreads);
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double> duration = end - start;
     std::cout << "\nExecution time: " << duration.count() << " seconds\n";
 
-    // Остановка потока мониторинга CPU
     monitoringCpu = false;
+
     WaitForSingleObject(cpuMonitorThread, INFINITE);
     CloseHandle(cpuMonitorThread);
 
-    double finalCpuUsage = getCpuUsage();
+    double finalCpuUsage = cpuMonitor.getCpuUsage();
     std::cout << "CPU load after execution: " << finalCpuUsage << "%\n";
 
-    // Вычисление среднего значения нагрузки на CPU
-    double averageCpuUsage = std::accumulate(cpuUsageValues.begin(), cpuUsageValues.end(), 0.0) / cpuUsageValues.size();
-    std::cout << "Average CPU load during execution: " << averageCpuUsage << "%\n";
+//    for (int i = 0; i < size;) {
+//        std:: cout << array[i] << " ";
+//        i = i + 10000;
+//    }
 
     return 0;
 }
